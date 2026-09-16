2f0930dbe3e89d9bdef55194d423bf7144692932
josh10e <j10e4077@gmail.com>
2026-09-16 17:11:27 -0600

    Generated toStrings for chessMove and chessPosition

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -31,4 +31,12 @@ public class ChessPosition {
     public int getColumn() {
         return col;
     }
+
+    @Override
+    public String toString() {
+        return "ChessPosition{" +
+                "row=" + row +
+                ", col=" + col +
+                '}';
+    }
 }
